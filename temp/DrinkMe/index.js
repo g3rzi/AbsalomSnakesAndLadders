781922cc89ea2c32d70f9b5ee08e0f5bd5434e56
AbsalomNargilotLTD <120471273+AbsalomNargilotLTD@users.noreply.github.com>
2022-12-13 15:11:05 +0200

    Add files via upload

---
+++ temp/DrinkMe/index.js
@@ -0,0 +1,39 @@
+const express      = require('express');
+const app          = express();
+const path         = require('path');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const nunjucks     = require('nunjucks');
+const routes       = require('./routes');
+const Database     = require('./database');
+
+
+const db = new Database('drinkme.db');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
+});
+
+app.set('views', './views');
+app.use('/static', express.static(path.resolve('static')));
+
+app.use(routes(db));
+
+app.all('*', (req, res) => {
+	return res.status(404).send({
+		message: '404 page not found'
+	});
+});
+
+(async () => {
+	await db.connect();
+	await db.migrate();
+	app.listen(4000, '0.0.0.0', () => console.log('Listening on port 4000'));
+})();
+
+module.export = db;
